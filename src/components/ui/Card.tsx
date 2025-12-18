e8e950611176d53ddbba45633a2645a0662f2f70
Filza Saleem <filza.saleem05@gmail.com>
2025-12-18 13:22:47 +0100

    feat:card component added
    Co-authored-by: Nora Silfver <nora.silfver@gmail.com>
    Co-authored-by: Marcus Grönnå <mgronna@gmail.com>
    Co-authored-by: Cristoffer Dessezar <dessezar@outlook.com>

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,5 +1,24 @@
 import React from "react";
-
+import { cn } from "../../lib/utils";
 export default function Card() {
 	return <div>Card</div>;
 }
+type propsType = {
+	className: string;
+	props: string[];
+};
+
+const cardHeader = ({ className, ...props }: propsType) => (
+	<div className={cn("flex flex-col space-y-1.5 p-6", className)} {...props}></div>
+);
+const cardTitle = ({ className, ...props }: propsType) => <div className={cn("text-sm text-muted-foreground", className)} {...props}></div>;
+const cardDescription = ({ className, ...props }: propsType) => (
+	<div className={cn("text-sm text-muted-foreground ", className)} {...props}></div>
+);
+const cardContent = ({ className, ...props }: propsType) => <div className={cn("p-6 pt-0", className)} {...props}></div>;
+const cardFooter = ({ className, ...props }: propsType) => <div className={cn("flex items-center p-6 pt-0", className)} {...props}></div>;
+const card = ({ className, ...props }: propsType) => (
+	<div className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)} {...props}></div>
+);
+
+export { cardHeader, cardTitle, cardDescription, cardContent, cardFooter, card };
