e216dae522642995212bac269aa7899348a79576
Sindri Elfarsson <sindrielfarsson.dev@gmail.com>
2025-12-19 15:03:03 +0100

    refactor: move RootLayour component to seperate file

---
+++ src/router/RootLayout.tsx
@@ -0,0 +1,31 @@
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { Outlet, useNavigate, useRouterState } from "@tanstack/react-router";
+import { useEffect } from "react";
+
+export const RootLayout = () => {
+	const navigate = useNavigate();
+	const { isSignedIn, sessionClaims, isLoaded } = useAuth();
+	const { location } = useRouterState();
+	const { user } = useUser();
+
+	useEffect(() => {
+		if (!isLoaded && !isSignedIn && location.pathname !== "/") {
+			navigate({ to: "/" });
+		}
+	}, [isLoaded, isSignedIn, location.pathname, navigate]);
+
+	useEffect(() => {
+		if (!isLoaded) return;
+		if (!isSignedIn) return;
+
+		const isAdmin = user?.publicMetadata.role === "admin";
+
+		if (isAdmin) {
+			navigate({ to: "/admin/dashboard" });
+		} else {
+			navigate({ to: "/patient/dashboard" });
+		}
+	}, [isSignedIn, sessionClaims, isLoaded, navigate]);
+
+	return <Outlet />;
+};
