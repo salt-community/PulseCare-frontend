f61afbc3614513f4ea9a4f18a640173a3984d538
Viktor Lundberg <vikkolundberg@gmail.com>
2025-12-19 14:43:53 +0100

    feat: create new routes based on role

--- src/router/dashboard.route.tsx
+++ src/router/dashboard.route.tsx
@@ -1,9 +1,9 @@
 import { createRoute } from "@tanstack/react-router";
-import { rootRoute } from "./rootRoute";
+import { patientRoute } from "./patient.route";
 import PatientDashboard from "../features/patient/dashboard/PatientDashboard";
 
 export const dashRoute = createRoute({
-	getParentRoute: () => rootRoute,
+	getParentRoute: () => patientRoute,
 	path: "/dashboard",
 	component: PatientDashboard
 });
