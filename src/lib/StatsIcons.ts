4a41e45ea1dce459f3e9798d8c2345f314ab9033
Filza Saleem <filza.saleem05@gmail.com>
2025-12-19 16:53:03 +0100

    created the statsicons

---
+++ src/lib/StatsIcons.ts
@@ -0,0 +1,10 @@
+import { Droplet, Gauge, HeartPulse, Scale } from "lucide-react";
+import { Activity } from "react";
+
+export const StatIcons = {
+    blood_pressure:Gauge,
+    glucose:Droplet,
+    cholesterol:Activity,
+    heart_rate:HeartPulse,
+    weight:Scale
+}
