edac8379422acc9179f0a9dbe449782d84a021da
Cristoffer Dessezar <dessezar@outlook.com>
2025-12-19 11:38:37 +0100

    feat: finished dashboard appointment
    co-authered-by: Marcus Grönnå <mgronna@gmail.com>

--- src/features/patient/dashboard/PatientDashboard.tsx
+++ src/features/patient/dashboard/PatientDashboard.tsx
@@ -23,38 +23,31 @@ export default function PatientDashboard() {
 				</CardTitle>
 				<div className="flex flex-col gap-3 mt-2">
 					{data.map(d => (
-						<Card key={d.id} className="p-3">
+						<Card key={d.id} className="p-3 bg-background-secondary">
 							<CardContent className="flex flex-col">
-								<div className="flex flex-col gap-4 m-2">
+								<div className="flex flex-col gap-4 mr-2">
 									<Pill className="h-5" variant="secondary">
 										<span className="">{d.type}</span>
 									</Pill>
-									<div className="flex flex-col gap-4 m-2">
+									<div className="flex flex-col gap-4 mr-2">
 										<span className="font-bold">
 											<Stethoscope className="inline size-4 mr-1.5 mb-1" />
 											{d.doctorName}
 										</span>
 									</div>
 								</div>
-								<div className="flex gap-4">
-									<div className="py-3 p-6 rounded-md">
-										<Calendar className="inline size-4" />
-										<span className="text-2xl font-bold">{format(new Date(d.date), "dd")}</span>
-										<span>{format(new Date(d.date), "MMM")}</span>
-									</div>
-									<div className="pt-2">
-										<span className="font-semibold">{format(new Date(d.date), "EEEE")}</span>
-										<span>
-											<Clock4 className="inline size-4 mr-1 mb-1" />
-											{d.time}
-										</span>
-									</div>
+								<div className=" border-b border-foreground/20 mt-2 pb-2 ">
+									<Calendar className="inline size-4 mr-1 mb-1" />
+									<span className="mr-1 text-sm">{format(new Date(d.date), "dd MMM")}</span>
+									<span className="mr-4 text-sm">{format(new Date(d.date), "EEEE")}</span>
+									<span>
+										<Clock4 className="inline size-4 mx-1 mb-1" />
+										{d.time}
+									</span>
 								</div>
-								<div className="flex flex-col md:flex-col gap-4 m-2">
+								<div className="flex flex-col md:flex-col gap-4 mt-2">
 									<span>{d.notes}</span>
 								</div>
-								{/* <br /> */}
-								{/* <br /> */}
 							</CardContent>
 						</Card>
 					))}
