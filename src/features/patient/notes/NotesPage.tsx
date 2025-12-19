831b8cd0d6a3032545952361949a4e8284ca3a0e
Patrik Lundström <patriklundstrom91@gmail.com>
2025-12-19 14:02:57 +0100

    feat: add notepage with notecards
    Co-authored-by: Felix Broberg <felix.broberg29@gmail.com>
    Co-authored-by: Johan Janérs <johanjaners@gmail.com>
    Co-authored-by: Sindri Elfarsson <sindrielfarsson.dev@gmail.com>

--- src/features/patient/notes/NotesPage.tsx
+++ src/features/patient/notes/NotesPage.tsx
@@ -1,3 +1,44 @@
+import { Card, CardContent } from "../../../components/ui/Card";
+import { mockNotes } from "../../../lib/api/mockData";
+import { Calendar, Stethoscope } from "lucide-react";
+
 export default function NotesPage() {
-	return <div>NotesPage</div>;
+	const data = mockNotes;
+
+	return (
+		<>
+			<h1 className="text-2xl font-bold">Appointment Notes</h1>
+			<p>Notes from your healthcare providers after appointments</p>
+			<div className="flex flex-col gap-3 mt-2">
+				{data.map(d => (
+					<Card key={d.id} className="p-2">
+						<CardContent className="flex flex-col gap-2 py-4 px-2 md:grid grid-cols-[4rem_auto_5.5rem] justify-stretch">
+							<div>
+								<span className="inline-flex items-center justify-center bg-secondary w-10 h-10 rounded-xl">
+									<Stethoscope className="w-4 h-4" strokeWidth={1.5} />
+								</span>
+							</div>
+							<div>
+								<div className="text-xl font-bold">{d.title}</div>
+								<div className="text-primary">{d.doctorName}</div>
+								<div>
+									<span className="font-bold">Note: </span>
+									{d.content}
+								</div>
+								<div>
+									<span className="font-bold">Diagnosis: </span>
+									{d.diagnosis}
+								</div>
+								<div className="flex gap-2 items-center bg-background rounded px-2">
+									<Calendar size={18} strokeWidth={1.5} />
+									{d.appointmentDetails}
+								</div>
+							</div>
+							<div>{d.date}</div>
+						</CardContent>
+					</Card>
+				))}
+			</div>
+		</>
+	);
 }
