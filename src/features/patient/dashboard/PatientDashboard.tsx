882d43c63aa406b8105fe09c6f316da5edace795
Nora Silfver <nora.silfver@gmail.com>
2025-12-22 13:53:14 +0100

    fix: improve UI consistency in PatientDashboard by updating doctor name display and adjusting layout

--- src/features/patient/dashboard/PatientDashboard.tsx
+++ src/features/patient/dashboard/PatientDashboard.tsx
@@ -31,22 +31,22 @@ export default function PatientDashboard() {
 										<span className="">{d.type}</span>
 									</Pill>
 									<div className="flex flex-col gap-4 mr-2">
-										<span className="font-medium">
-											<Stethoscope className="inline size-4 mr-1.5 mb-1" />
+										<span className="text-foreground text-sm font-semibold">
+											<Stethoscope className="text-card-foreground inline size-4 mr-1.5 mb-1" />
 											{d.doctorName}
 										</span>
 									</div>
 								</div>
-								<div className=" border-b border-foreground/20 mt-2 pb-2 ">
-									<Calendar className="inline size-4 mr-1 mb-1" />
+								<div className="border-b border-foreground/20 mt-2 pb-2 flex flex-row items-center">
+									<Calendar className="inline size-4 mr-1" />
 									<span className="mr-1 text-sm">{format(new Date(d.date), "dd MMM")}</span>
 									<span className="mr-4 text-sm">{format(new Date(d.date), "EEEE")}</span>
-									<span>
-										<Clock4 className="inline size-4 mx-1 mb-1" />
+									<span className="flex flex-row text-sm items-center">
+										<Clock4 className="inline size-4 mx-1" />
 										{d.time}
 									</span>
 								</div>
-								<div className="flex flex-col md:flex-col gap-4 mt-2">
+								<div className="flex flex-col md:flex-col gap-4 mt-2 text-xs">
 									<span>{d.notes}</span>
 								</div>
 							</CardContent>
