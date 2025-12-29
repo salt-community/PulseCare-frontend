cd2f491629a26d213931d3479e6a63b0b5b35c9f
Viktor Lundberg <vikkolundberg@gmail.com>
2025-12-29 10:20:45 +0100

    feat: add basic admin dashboard layout

--- src/features/admin/dashboard/AdminDashboardPage.tsx
+++ src/features/admin/dashboard/AdminDashboardPage.tsx
@@ -1,3 +1,29 @@
+import { Calendar, Users } from "lucide-react";
+import PageHeader from "../../../components/shared/PageHeader";
+import { Card, CardTitle } from "../../../components/ui/Card";
+
 export default function AdminDashboardPage() {
-	return <div>AdminDashboard</div>;
+	const exampleUser = {
+		fullName: "John Doe"
+	};
+
+	return (
+		<>
+			<PageHeader title={`Welcome back ${exampleUser.fullName}`} description="Here's an overview of today's activities" />
+			<div className="flex gap-6">
+				<Card className="flex-1 mt-6 p-4 shadow-none hover:shadow-none">
+					<CardTitle className="flex items-center gap-2 text-foreground p-3 text-lg md:text-xl lg:text-2xl">
+						<Users className="text-primary shrink-0" size={20} />
+						Recent Patients
+					</CardTitle>
+				</Card>
+				<Card className="flex-1 mt-6 p-4 shadow-none hover:shadow-none">
+					<CardTitle className="flex items-center gap-2 text-foreground p-3 text-lg md:text-xl lg:text-2xl">
+						<Calendar className="text-primary shrink-0" size={20} />
+						Upcoming Appointments
+					</CardTitle>
+				</Card>
+			</div>
+		</>
+	);
 }
