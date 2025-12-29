25107793b460289c00d29241969eea1d51bc6888
Felix Broberg <felix.broberg29@gmail.com>
2025-12-29 17:17:01 +0100

    feat: add appointment schedule logic
    Co-authored-by: Johan Janérs <johanjaners@gmail.com>
    Co-authored-by: Patrik Lundström <patriklundstrom91@gmail.com>
    Co-authored-by: Sindri Elfarsson <sindrielfarsson.dev@gmail.com>

--- src/features/admin/calendar/AdminCalendarPage.tsx
+++ src/features/admin/calendar/AdminCalendarPage.tsx
@@ -6,9 +6,11 @@ import { format } from "date-fns";
 import { Calendar } from "lucide-react";
 import { ScheduleAppointment } from "./ScheduleAppointment";
 import PageHeader from "../../../components/shared/PageHeader";
+import { mockAppointments } from "../../../lib/api/mockData";
 
 export const AdminCalendarPage = () => {
 	const [selected, setSelected] = useState<Date>();
+	const appointments = mockAppointments;
 	return (
 		<>
 			<div className="flex justify-between items-center">
@@ -20,14 +22,19 @@ export const AdminCalendarPage = () => {
 				<div className="flex-1 flex flex-col gap-4">
 					<h2 className="flex items-center gap-2 text-2xl font-semibold">
 						<Calendar className="h-6 text-primary" />
+
 						{selected ? format(selected, "EEEE dd MMMM yyyy", { locale: enGB }) : "Select a date"}
 					</h2>
-					<Card className="p-10">
-						<CardContent className="flex items-center gap-4 flex-col">
-							<Calendar className="h-10 w-50" />
-							No appointments on this date
-						</CardContent>
-					</Card>
+					{!selected || appointments.filter(apt => apt.date === format(selected, "yyyy-MM-dd", { locale: enGB })).length === 0 ? (
+						<Card className="p-10">
+							<CardContent className="flex items-center gap-4 flex-col">
+								<Calendar className="h-10 w-50" />
+								No appointments on this date
+							</CardContent>
+						</Card>
+					) : (
+						<div>hej</div>
+					)}
 				</div>
 			</div>
 		</>
