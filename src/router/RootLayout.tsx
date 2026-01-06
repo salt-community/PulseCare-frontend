ccb85f9c0aed388d7f4541e165d4dc8740116437
shakana0 <shakanajamal3@gmail.com>
2026-01-06 22:06:45 +0100

    feat: add ChatProvider

--- src/router/RootLayout.tsx
+++ src/router/RootLayout.tsx
@@ -1,12 +1,18 @@
 import { useAuth, useUser } from "@clerk/clerk-react";
 import { Outlet, useNavigate, useRouterState } from "@tanstack/react-router";
 import { useEffect } from "react";
+import { ChatProvider } from "../context/ChatContext";
 
 export const RootLayout = () => {
 	const navigate = useNavigate();
-	const { isSignedIn, isLoaded } = useAuth();
+	const { isSignedIn, isLoaded, getToken } = useAuth();
 	const { location } = useRouterState();
 	const { user } = useUser();
+
+	const isAdmin = user?.publicMetadata.role === "admin";
+	const role = isAdmin ? "doctor" : "patient";
+
+	const hardcodedId = isAdmin ? "067fa0de-2b36-4368-a491-604a73454c23" : "183977c2-a4a1-408b-89fb-7d4a136738df";
 
 	useEffect(() => {
 		if (!isLoaded) return;
@@ -32,5 +38,10 @@ export const RootLayout = () => {
 		}
 	}, [isLoaded, isSignedIn, navigate, location.pathname, user?.publicMetadata.role]);
 
-	return <Outlet />;
+	if (!isLoaded) return null;
+	return (
+		<ChatProvider token={async () => (await getToken()) ?? ""} role={role} userId={hardcodedId}>
+			<Outlet />
+		</ChatProvider>
+	);
 };
